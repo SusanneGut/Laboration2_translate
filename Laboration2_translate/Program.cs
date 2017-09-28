084a324a14253243b30bb313482040fbde6e8c42
gutsusanne <gutsusanne@gmail.com>
2017-09-28 22:10:42 +0200

    uppdaterad

--- Laboration2_translate/Program.cs
+++ Laboration2_translate/Program.cs
@@ -3,67 +3,103 @@ using System.Collections.Generic;
 
 namespace Laboration2_translate
 {
-    class Program
+    public class Program
     {
         static string animalSwe;
-        static string animalSpa;
         static List<string> ListSwe;
         static List<string> ListSpa;
         private static int input;
 
-
         public static void SwedishList()
         {
-
+            ListSwe = new List<string>();
+            {
+                ListSwe.Add("katt");
+                ListSwe.Add("hund");
+                ListSwe.Add("kanin");
+                ListSwe.Add("häst");
+                ListSwe.Add("ko");
+                ListSwe.Add("gris");
+                ListSwe.Add("höna");
+            };
 
         }
 
         public static string SelectAnimal()
         {
 
-            return "";
+            Console.WriteLine("Skriv in djur: ");
+            animalSwe = Console.ReadLine();
+
+            return animalSwe;
         }
 
         public static bool SearchSweList()
         {
-
-            return true;
+            if (ListSwe.Contains(animalSwe))
+                return true;
             return false;
+
+
         }
 
         public static void IndexPosSwe()
         {
+            if (ListSwe.Contains(animalSwe))
+
+                input = ListSwe.IndexOf(animalSwe);
 
         }
-
         public static void SpanishList()
         {
-
+            ListSpa = new List<string>();
+            {
+                ListSpa.Add("gato");
+                ListSpa.Add("perro");
+                ListSpa.Add("conejo");
+                ListSpa.Add("caballo");
+                ListSpa.Add("vaca");
+                ListSpa.Add("cerdo");
+                ListSpa.Add("gallina");
+            };
         }
-
         public static string IndexPosSpa(int input)
         {
 
-            return "";
+
+            return ListSpa[input];
+
         }
 
         public static void Output()
         {
 
-        }
+            Console.WriteLine(animalSwe + " på spanska heter " + IndexPosSpa(input) + ".");
+            Console.WriteLine("Vill du översätta ett nytt djur? J/N");
+            string answer = Console.ReadLine();
+            if (answer == "J")
+            {
+                Main();
+            }
 
-        static void Main()
+            else Console.WriteLine("Tack för idag!");
+
+        }
+        public static void Main()
         {
             SwedishList();
             SelectAnimal();
-            SearchSweList();
-            IndexPosSwe();
-            Console.WriteLine(input);
-            SpanishList();
-            Output();
-
-
-
+            if (SearchSweList())
+            {
+                IndexPosSwe();
+                SpanishList();
+                Output();
+            }
+            else
+            {
+                Console.WriteLine("Ditt djur finns inte på listan.");
+                Main();
+            }
         }
     }
 }
