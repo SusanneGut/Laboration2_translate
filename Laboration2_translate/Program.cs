ef58c95ae8b9c07ff5197e6ad3b6069e11480ea7
gutsusanne <gutsusanne@gmail.com>
2017-09-28 20:56:22 +0200

    instansvariabler

--- Laboration2_translate/Program.cs
+++ Laboration2_translate/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Laboration2_translate
 {
     class Program
     {
-        static void Main(string[] args)
+        static string animalSwe;
+        static string animalSpa;
+        static List<string> ListSwe;
+        static List<string> ListSpa;
+        private static int input;
+
+
+        public static void SwedishList()
         {
-            Console.WriteLine("Hello World!");
+
+
+        }
+
+        public static string SelectAnimal()
+        {
+
+            return "";
+        }
+
+        public static bool SearchSweList()
+        {
+
+            return true;
+            return false;
+        }
+
+        public static void IndexPosSwe()
+        {
+
+        }
+
+        public static void SpanishList()
+        {
+
+        }
+
+        public static string IndexPosSpa(int input)
+        {
+
+            return "";
+        }
+
+        public static void Output()
+        {
+
+        }
+
+        static void Main()
+        {
+            SwedishList();
+            SelectAnimal();
+            SearchSweList();
+            IndexPosSwe();
+            Console.WriteLine(input);
+            SpanishList();
+            Output();
+
+
+
         }
     }
 }
