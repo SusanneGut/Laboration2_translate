659e62fb92237cd7563a7a6bb8fd381446cd6f77
gutsusanne <gutsusanne@gmail.com>
2017-09-28 20:30:42 +0200

    Add project files.

---
+++ Laboration2_translate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboration2_translate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
